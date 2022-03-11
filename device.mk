9eb8108ccbad453ec1130b16e0dcb07d9e02623b
AIV-97 <AIV-97@users.noreply.github.com>
2022-03-11 10:20:36 +0700

    RMX1941: Don't build libbluetooth_audio_session.vendor
    * we have libbluetooth_audio_session-mediatek
    
    Signed-off-by: AIV-97 <AIV-97@users.noreply.github.com>
    Change-Id: I482b2144756d6c3e72d01109879b48efff163c79

--- device.mk
+++ device.mk
@@ -86,7 +86,6 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth.a2dp@1.0 \
     android.hardware.bluetooth.a2dp@1.0.vendor \
     android.hardware.bluetooth@1.0.vendor \
-    libbluetooth_audio_session.vendor \
     libbtconfigstore
 
 # Camera
